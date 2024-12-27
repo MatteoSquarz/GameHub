580c32fb693830804b32043c232de8259fd7ad0b
MatteoSquarz <93669086+MatteoSquarz@users.noreply.github.com>
2024-12-27 21:36:21 +0100

    aggiunta profileIcon in header

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 $paginaHTML = file_get_contents('index.html');
 //$menuLoginProfilo = $_SESSION['username'];
-$menuLoginProfilo = "<li><a href=\"login.php\">Accedi</a></li>";
+$menuLoginProfilo = "<li class=\"login\"><a href=\"login.php\">Accedi</a></li>";
 session_start();
 if (!empty($_SESSION)) 
-    $menuLoginProfilo = "<li><a href=\"profilo.php\">Profilo</a></li>";
+    $menuLoginProfilo = "<li class=\"profile\"><a href=\"profilo.php\">Profilo</a></li>";
 
 $paginaHTML = str_replace('[loginProfilo]', $menuLoginProfilo, $paginaHTML);
 echo $paginaHTML;
--- profilo.php
+++ profilo.php
@@ -1,10 +1,10 @@
 <?php
 $paginaHTML = file_get_contents('profilo.html');
 //$menuLoginProfilo = $_SESSION['username'];
-$menuLoginProfilo = "<li><a href=\"login.php\">Accedi</a></li>";
+$menuLoginProfilo = "<li class=\"login\"><a href=\"login.php\">Accedi</a></li>";
 session_start();
 if (!empty($_SESSION)) 
-    $menuLoginProfilo = "<li id=\"currentMenu\">Profilo</a></li>";
+    $menuLoginProfilo = "<li id=\"currentMenu\" class=\"profile\">Profilo</li>";
 
 $paginaHTML = str_replace('[loginProfilo]', $menuLoginProfilo, $paginaHTML);
 echo $paginaHTML;
