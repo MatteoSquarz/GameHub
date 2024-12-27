3c96b24375bc12d8306bb0e09865b002d2b07222
Andrea Piola <massiandrea24@gmail.com>
2024-12-27 16:32:25 +0100

    Aggiunta sessione con modifica index

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 $paginaHTML = file_get_contents('index.html');
+//$menuLoginProfilo = $_SESSION['username'];
+$menuLoginProfilo = "<li><a href=\"login.php\">Accedi</a></li>";
+session_start();
+if (!empty($_SESSION)) 
+    $menuLoginProfilo = "<li><a href=\"profilo.php\">Profilo</a></li>";
+
+$paginaHTML = str_replace('[loginProfilo]', $menuLoginProfilo, $paginaHTML);
 echo $paginaHTML;
 ?>
