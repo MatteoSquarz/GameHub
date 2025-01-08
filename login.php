52c24cc5db64f5d435f5444cd302e7e12a5800e9
MatteoSquarz <93669086+MatteoSquarz@users.noreply.github.com>
2025-01-08 11:01:55 +0100

    fix login

--- login.php
+++ login.php
@@ -16,16 +16,9 @@ if (isset($_POST['accedi'])) {
 	$messaggiPerForm .= "<ul class=\"errorLogin\">";
 	
     $username = $connessione->pulisciInput($_POST['username']);
-	/*if(!preg_match("/^[a-zA-Z0-9]{2,}$/",$username))
-        $messaggiPerForm .= "<li>Non è sicuramente uno username</li>";*/
-
     $password = $connessione->pulisciInput($_POST['password']);
-	/*if(!preg_match("/^[A-Za-z0-9\!\@\#\%]{4,}$/",$password))
-        $messaggiPerForm .= "<li>Non è sicuramente una password</li>";*/
-	
 	
 	if($connessioneOK == NULL){
-		$autenticazione = $connessione->autenticaUtente($username,$password);
 		if($connessione->autenticaUtente($username,$password)){
 			session_start();
 			if(empty($_SESSION)){
