7bb8016554f1670351850e6baa60dddd84a4244c
MatteoSquarz <93669086+MatteoSquarz@users.noreply.github.com>
2025-01-08 16:59:18 +0100

    pagina admin

---
+++ admin.php
@@ -0,0 +1,35 @@
+<?php
+require_once "templatedbConnection.php";
+use DB\DBAccess;
+
+$paginaHTML = file_get_contents('admin.html');
+
+//fare un controllo se admin o no
+
+
+$connection = new DBAccess();
+$connectionOK = $connection->openDBConnection();
+
+$abbonamenti = "";
+$listaAbbonamenti = "";
+$categorie = "";
+$listaCategorie = "";
+$piattaforme = "";
+$listaPiattaforme = "";
+
+if(!$connectionOK){
+    $abbonamenti = $connection->getListAbbonamenti();
+    $categorie = $connection->getListCategorie();
+    $piattaforme = $connection->getListPiattaforme();
+    $connection->closeConnection();
+
+
+    foreach($abbonamenti as $abbonamento){
+        $nome = $abbonamento['nome'];
+        $listaAbbonamenti .= "<option value=\"$nome\">$nome</option>";
+    }
+
+
+}
+
+$paginaHTML = str_replace('[listaAbbonamenti]', $listaAbbonamenti, $paginaHTML);
