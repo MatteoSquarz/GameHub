1cd660e05eadeabbee97711c13440cd15f1d8064
francesco-lapenna <francesco.lapenna.d@gmail.com>
2025-01-13 09:36:02 +0100

    aggiunti drop (tabelle già ordinate)

--- database.sql
+++ database.sql
@@ -1,3 +1,27 @@
+/*
+   _____                    _                    _______    _          _ _
+  / ____|                  (_)                  |__   __|  | |        | | |
+ | |     _ __ ___  __ _ _____  ___  _ __   ___     | | __ _| |__   ___| | | ___
+ | |    | '__/ _ \/ _` |_  / |/ _ \| '_ \ / _ \    | |/ _` | '_ \ / _ \ | |/ _ \
+ | |____| | |  __/ (_| |/ /| | (_) | | | |  __/    | | (_| | |_) |  __/ | |  __/
+  \_____|_|  \___|\__,_/___|_|\___/|_| |_|\___|    |_|\__,_|_.__/ \___|_|_|\___|
+
+*/
+
+DROP TABLE IF EXISTS AbbonamentoVideogioco;
+DROP TABLE IF EXISTS PiattaformaVideogioco;
+DROP TABLE IF EXISTS CategoriaVideogioco;
+DROP TABLE IF EXISTS StoricoAbbonamento;
+DROP TABLE IF EXISTS Vendita;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Admin;
+DROP TABLE IF EXISTS Categoria;
+DROP TABLE IF EXISTS Piattaforma;
+DROP TABLE IF EXISTS Videogioco;
+DROP TABLE IF EXISTS Abbonamento;
+DROP TABLE IF EXISTS Utente;
+
+
 CREATE TABLE Utente(
     username varchar(20) NOT NULL,
     password varchar(20) NOT NULL,
@@ -101,7 +125,17 @@ CREATE TABLE AbbonamentoVideogioco(
     FOREIGN KEY(videogioco) REFERENCES Videogioco(codice)
 );
 
-/* ------------------------------------------------------------------------------ */
+/*
+  _____                  _                            _          _______    _          _ _
+ |  __ \                | |                          | |        |__   __|  | |        | | |
+ | |__) |__  _ __   ___ | | __ _ _ __ ___   ___ _ __ | |_ ___      | | __ _| |__   ___| | | ___
+ |  ___/ _ \| '_ \ / _ \| |/ _` | '_ ` _ \ / _ \ '_ \| __/ _ \     | |/ _` | '_ \ / _ \ | |/ _ \
+ | |  | (_) | |_) | (_) | | (_| | | | | | |  __/ | | | || (_) |    | | (_| | |_) |  __/ | |  __/
+ |_|   \___/| .__/ \___/|_|\__,_|_| |_| |_|\___|_| |_|\__\___/     |_|\__,_|_.__/ \___|_|_|\___|
+            | |
+            |_|
+*/
+
 INSERT INTO Utente (username, password) VALUES
 ('user','user'),
 ('admin','admin');
